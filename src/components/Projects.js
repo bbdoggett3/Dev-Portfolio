42ad3fb3c0388a658c8e4ab85fcc3aa69340ea02
bbdoggett3 <bbdoggett3@gmail.com>
2020-08-10 11:06:30 -0600

    create projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,25 @@ function Projects() {
         <div className="projects-container">
             <div className="projects-content">
                 <h2 className="projects-heading">Projects</h2>
+                <div className="projects">
+                    <div className="projects-info">
+                        <img
+                            className="project-img" 
+                            src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2FProfile%20Couplely.png?v=1595274007785"
+                            alt="hearttaken website" 
+                        />
+                        <h2 className="project-title">HeartTaken</h2>
+                    </div>
+                    <div className="projects-info">
+                        <img
+                            className="project-img" 
+                            src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2FLanding%20FINAL%20component.png?v=1595264619244"
+                            alt="codepound website" 
+                        />
+                        <h2 className="project-title">Codepound</h2>                        
+                    </div>
+
+                </div>
             </div>
         </div>
     )
