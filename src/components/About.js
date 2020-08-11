9fc00a09ef77eaf813dd8260063820d05fbf0ed2
bbdoggett3 <bbdoggett3@gmail.com>
2020-08-11 15:35:02 -0600

    create about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 function About() {
     return(
-        <div>This is the ABOUT page</div>
+        <div className="about-container">
+            <div className="about-content">
+                <h2 className="about-heading">About</h2>
+                <div className="about">
+                    <img
+                        className="about-img"
+                        src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2FBen%20Squared.png?v=1597181570543"
+                        alt="Smiling Developer"
+                    
+                    />
+
+                    <p className="about-info"></p>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
