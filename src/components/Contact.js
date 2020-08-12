d0f223b3ba5a732771011631b6388d2ae2a9bdf1
bbdoggett3 <bbdoggett3@gmail.com>
2020-08-12 13:26:52 -0600

    create footer component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,27 @@ function Contact() {
         <div className="contact-container">
             <div className="contact-content">
                 <h2 className="contact-heading">Contact</h2>
-
+                <input
+                    className="input-field"
+                    type="text"
+                    placeholder="Name"  
+                />
+                <input
+                    className="input-field"
+                    type="email"
+                    placeholder="Email"  
+                />
+                <input
+                    className="input-field"
+                    type="email"
+                    placeholder="Company"  
+                />
+                <textarea
+                    className="text-box"
+                    type="text"
+                    placeholder="Addtional questions you might have for me..."
+                />
+                <button className="submit-btn">Submit</button>
             </div>
         </div>
     )
