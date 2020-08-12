16747fcbbdcb956e9ed934407e61d8b2dbcfe1e9
bbdoggett3 <bbdoggett3@gmail.com>
2020-08-12 13:04:36 -0600

    create about page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function Contact() {
     return(
-        <div>This is the CONTACT page</div>
+        <div className="contact-container">
+            <div className="contact-content">
+                <h2 className="contact-heading">Contact</h2>
+
+            </div>
+        </div>
     )
 }
 
