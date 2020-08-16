b842e7cf12e705dcec65c4a7e92d7b6a095095b5
bbdoggett3 <bbdoggett3@gmail.com>
2020-08-15 23:51:59 -0600

    create LEARN MORE btn on about component

--- src/components/About.js
+++ src/components/About.js
@@ -16,6 +16,7 @@ function About() {
                     <p className="about-info">My name is Benjamin Doggett, I recently graduated from DevMountain as a certified Full stack web developer, and Brigham Young University- Idaho with a Bachelors's degree in Software Engineering. I have high hopes to further that degree/skills with industry experience along with self-taught applications of new technologies. I'm a Proven leader with more than 8 years’ experience in managerial roles, administration, training, and facilitating demands/ goals. A results-oriented, hardworking, resourceful leader with the ability to manage in a diverse environment. Experienced in balancing the needs of the individual and company demands. Skilled communicator and trainer. Proficient at flexing and bending with change. Excels in a team environment, as well as hiring and building team relationships to accomplish organizational programming objectives and goals.
                     </p>
                 </div>
+                <button>Learn More</button>
             </div>
 
         </div>
