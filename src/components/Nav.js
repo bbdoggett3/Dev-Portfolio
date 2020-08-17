b34b85a5c7688c174f2f64ac3621fd6f4d0cc7c8
bbdoggett3 <bbdoggett3@gmail.com>
2020-08-17 10:05:22 -0600

    create a new projects component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ function Nav() {
     return(
         <div className="nav-container">
             <ul className="nav-list">
-                <Link style={{ textDecoration: "none" }} to="/skillsMore">
+                <Link style={{ textDecoration: "none", color: "#faf9ed" }} to="/skillsMore">
                     <li className="list-item">Skills</li>
                 </Link>
                 <li className="list-item-break">|</li>
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-import App from "./App";
 import SkillsMore from './components/SkillsMore';
-import Projects from './components/Projects';
+import ProjectsMore from './components/ProjectsMore';
 import About from './components/About';
 import Contact from './components/Contact';
 import Welcome from './components/Welcome';
@@ -10,9 +9,8 @@ import Welcome from './components/Welcome';
 export default(
     <Switch>
         <Route exact path="/" component={Welcome}/>
-        {/* <Route path="/welcome" component={Welcome}/> */}
         <Route path="/skillsMore" component={SkillsMore}/>
-        <Route path="/projects" component={Projects}/>
+        <Route path="/projectsMore" component={ProjectsMore}/>
         <Route path="/about" component={About}/>
         <Route path="/contact" component={Contact}/>
     </Switch>
