a60f287c8013622f0245fa8e245c19b7c0cd2c31
bbdoggett3 <bbdoggett3@gmail.com>
2020-08-17 11:21:32 -0600

    create new contact component

--- src/components/contactMore.js
+++ src/components/contactMore.js
@@ -1,32 +1,50 @@
 import React from 'react';
+import Footer from './Footer';
+import Nav from './Nav';
 
 function ContactMore() {
     return(
-        <div className="contact-container">
-            <div className="contact-content">
-                <h2 className="contact-heading">Contact</h2>
-                <input
-                    className="input-field"
-                    type="text"
-                    placeholder="Name"  
-                />
-                <input
-                    className="input-field"
-                    type="email"
-                    placeholder="Email"  
-                />
-                <input
-                    className="input-field"
-                    type="email"
-                    placeholder="Company"  
-                />
-                <textarea
-                    className="text-box"
-                    type="text"
-                    placeholder="Addtional questions you might have for me..."
-                />
-                <button className="submit-btn">Submit</button>
+        <div className="contact-more-container">
+            <Nav/>
+            <h2 className="contact-more-heading">Contact</h2>
+            <div className="contact-more-page">
+                <div className="contact-more-general-section">
+                    <p className="contact-more-info">If you have any questions regarding my technical experience, projects, or would just like to get to know me better. Please reach out to me by filling in the form on your right. Can’t wait to meet you!</p>
+                    <img
+                        className="contact-more-img"
+                        src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2FFlying-Mail-Black-Icon.png?v=1597683299097"
+                        alt="flyinf paper airplane"
+                    
+                    />
+                </div>
+
+                <div className="contact-more-content">
+                    <p className="contact-more-please">*Please fill out the information below, thank you!</p>
+                    <input
+                        className="more-input-field"
+                        type="text"
+                        placeholder="Name"  
+                    />
+                    <input
+                        className="more-input-field"
+                        type="email"
+                        placeholder="Email"  
+                    />
+                    <input
+                        className="more-input-field"
+                        type="email"
+                        placeholder="Company"  
+                    />
+                    <textarea
+                        className="more-text-box"
+                        type="text"
+                        placeholder="Addtional questions you might have for me..."
+                    />
+                    <button className="more-submit-btn">Submit</button>
+                </div>
+
             </div>
+            <Footer/>
         </div>
     )
 }
