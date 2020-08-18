be3219e96730e287c5297859edf05f9e57d641d1
bbdoggett3 <bbdoggett3@gmail.com>
2020-08-18 12:39:13 -0600

    create a nav HOME btn

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,6 +4,13 @@ import { Link } from 'react-router-dom';
 function Nav() {
     return(
         <div className="nav-container">
+             <Link to="/">
+                <img
+                    className="nav-home-img" 
+                    src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2FBob%20tran.png?v=1597085547572" 
+                    alt="profile of Developer"
+                />
+             </Link>
             <ul className="nav-list">
                 <Link style={{ textDecoration: "none", color: "#faf9ed" }} to="/skillsMore">
                     <li className="list-item">Skills</li>
