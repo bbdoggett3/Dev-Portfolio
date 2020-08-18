7eec9a62df45823ae2678471f4bf63da811456c5
bbdoggett3 <bbdoggett3@gmail.com>
2020-08-18 14:25:28 -0600

    create social media links on contact page

--- src/components/contactMore.js
+++ src/components/contactMore.js
@@ -31,13 +31,38 @@ function ContactMore() {
             <h2 className="contact-more-heading">Contact</h2>
             <div className="contact-more-page">
                 <div className="contact-more-general-section">
-                    <p className="contact-more-info">If you have any questions regarding my technical experience, projects, or would just like to get to know me better. Please reach out to me by filling in the form on your right. Can’t wait to meet you!</p>
+                    <p className="contact-more-info">If you have any questions regarding my technical experience, projects, or would just like to get to know me better. Please reach out to me by filling in the form on your right, or clicking the social media links below. Can’t wait to meet you!</p>
                     <img
                         className="contact-more-img"
                         src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2FFlying-Mail-Black-Icon.png?v=1597683299097"
                         alt="flyinf paper airplane"
                     
                     />
+                    <div className="social-media-links">
+                        <a href="https://github.com/bbdoggett3">
+                            <img
+                                className="welcome-icon-image" 
+                                src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Fgithub-153-675523.png?v=1596049618612" 
+                                alt="github icon"
+                            />
+                        </a>
+
+                        <a href="https://www.linkedin.com/in/benjamin-doggett/">
+                            <img
+                                className="welcome-icon-image-linkedin" 
+                                src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Flinkedin-32-114604.png?v=1596049859369" 
+                                alt="linkedIn icon"
+                            />
+                        </a>
+
+                        <a href="https://www.facebook.com/ben.doggett.12/">
+                            <img
+                                className="welcome-icon-image" 
+                                src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Ffacebook%20black.png?v=1596050184365" 
+                                alt="Facebook icon"
+                            />
+                        </a>
+                    </div>
                 </div>
 
                 <div className="contact-more-content">
