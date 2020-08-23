bee815ec78ef61ec3e70e0c330ddac8b14d66c76
bbdoggett3 <bbdoggett3@gmail.com>
2020-08-22 21:24:32 -0600

    Create new ABOUT component/wireframe

--- src/components/AboutMore.js
+++ src/components/AboutMore.js
@@ -8,22 +8,34 @@ function AboutMore() {
             <Nav/>
             <div className="about-more-content">
                 <div className="about-more">
-                    <img
-                        className="about-more-img"
-                        src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2FBen%20Squared.png?v=1597181570543"
-                        alt="Smiling Developer"
-                    
-                    />
                     <div className="btn-column">
+
                         <div className="btn-content">
+                            <img
+                                className="about-more-img"
+                                src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Fpro%20person.png?v=1598151002045"
+                                alt="business casual developer"
+                                />
                             <button className="learn-more-about-more-btn">Professional Life</button>
                         </div>
                         <div className="btn-content">
+                            <img
+                                className="about-more-img"
+                                src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Fperson.png?v=1598150993930"
+                                alt="Casual dressed developer"
+                            />
                         <button className="learn-more-about-more-btn">Personal Life</button>
                         </div>
+                        <div className="btn-content">
+                            <img
+                                className="about-more-img"
+                                src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Ffile%20Icon.png?v=1598151288760"
+                                alt="Resume Icon"
+                            />
                     <a href="./doc/Benjamin Doggett Technical Resume.docx" download>
                         <button className="learn-more-about-more-btn">Download Resume</button>
                     </a>
+                    </div>
                     </div>
                 </div>
             </div>
