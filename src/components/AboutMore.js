e3ab3504e63be311b7ef028c1e9b46c3364f4499
bbdoggett3 <bbdoggett3@gmail.com>
2020-08-25 15:12:50 -0600

    create a toggle view on the ABOUT page

--- src/components/AboutMore.js
+++ src/components/AboutMore.js
@@ -1,33 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
 
 function AboutMore() {
+    const [showProfessional, setShowProfessional] = useState(false);
+    const [showPersonal, setShowPersonal] = useState(false);
+    
+    function toggleShowProfessional() {
+         setShowProfessional(!showProfessional);
+    }
+
+    function toggleShowPersonal() {
+         setShowPersonal(!showPersonal);
+    }
+
     return(
         <div className="about-more-container">
             <Nav/>
             <div className="about-more-content">
                 <div className="about-more">
                     <div className="btn-column">
-
+                        {/* Professional */}
                         <div className="btn-content">
                             <img
                                 className="about-more-img"
                                 src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Fpro%20person.png?v=1598151002045"
                                 alt="business casual developer"
                                 />
-                            <button className="learn-more-about-more-btn">Professional Life</button>
+                            <button
+                            onClick={() => toggleShowProfessional()}
+                            className="learn-more-about-more-btn">Professional Life</button>
                         </div>
+
+                        {/* Personal */}
                         <div className="btn-content">
                             <img
                                 className="about-more-img"
                                 src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Fperson.png?v=1598150993930"
                                 alt="Casual dressed developer"
                             />
-                        <button className="learn-more-about-more-btn">Personal Life</button>
+                        <button 
+                        onClick={() => toggleShowPersonal()}
+                        className="learn-more-about-more-btn">Personal Life</button>
                         </div>
                         <div className="btn-content">
-                        {/* Icons made by <a href="https://icon54.com/" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>  */}
                             <img
                                 className="about-more-img"
                                 src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Ffile%20Icon.png?v=1598151288760"
@@ -39,10 +55,22 @@ function AboutMore() {
                     </div>
                     </div>
                 </div>
+                    <div className= {`${showProfessional ? "about-open" : "about-closed"}`}>
+                         <div className="about-open-container">
+                            <img
+                                className="about-open-img"
+                                src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Ffile%20Icon.png?v=1598151288760"
+                                alt="Resume Icon"
+                                />
+                            <p>This is open</p>
+                         </div>
+                    </div>
             </div>
         <Footer/>
+        {/* Icons made by <a href="https://icon54.com/" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>  */}
         </div>
     )
 }
 
 export default AboutMore;
+
