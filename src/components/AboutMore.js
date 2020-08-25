7ebaf9a28ebbdf8be1a7db9441ce79300333c752
bbdoggett3 <bbdoggett3@gmail.com>
2020-08-25 15:38:52 -0600

    add some style to my ABOUT component

--- src/components/AboutMore.js
+++ src/components/AboutMore.js
@@ -62,17 +62,17 @@ function AboutMore() {
                                 src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2FBen%20Squared.png?v=1597181570543"
                                 alt="Resume Icon"
                                 />
-                            <p>This is Professional open</p>
+                            <p className="about-open-para">My name is Benjamin Doggett, I recently graduated from DevMountain as a certified Full stack web developer, & Brigham Young University- Idaho with a Bachelors's degree in Software Engineering. I have high hopes to further that degree/skills with industry experience along with self-taught applications of new technologies. I'm a Proven leader with more than 8 years’ experience in managerial roles, administration, training, and facilitating demands/ goals. A results-oriented, hardworking, resourceful leader with the ability to manage in a diverse environment. Experienced in balancing the needs of the individual and company demands. Skilled communicator and trainer. Proficient at flexing and bending with change. Excels in a team environment, as well as hiring and building team relationships to accomplish organizational programming objectives and goals.</p>
                          </div>
                     </div>
                     <div className= {`${showPersonal ? "about-open" : "about-closed"}`}>
                          <div className="about-open-container">
                             <img
-                                className="about-open-img"
+                                // className="about-open-img"
                                 src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2FBen%20and%20Liv%20family%202020.jpg?v=1598390437998"
                                 alt="Resume Icon"
                                 />
-                            <p>This is Personal open</p>
+                            <p className="about-open-para">My name is Benjamin Doggett, I recently graduated from DevMountain as a certified Full stack web developer, & Brigham Young University- Idaho with a Bachelors's degree in Software Engineering. I have high hopes to further that degree/skills with industry experience along with self-taught applications of new technologies. I'm a Proven leader with more than 8 years’ experience in managerial roles, administration, training, and facilitating demands/ goals. A results-oriented, hardworking, resourceful leader with the ability to manage in a diverse environment. Experienced in balancing the needs of the individual and company demands. Skilled communicator and trainer. Proficient at flexing and bending with change. Excels in a team environment, as well as hiring and building team relationships to accomplish organizational programming objectives and goals.</p>
                          </div>
                     </div>
             </div>
